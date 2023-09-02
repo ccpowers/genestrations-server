496eab25845b13bd204859d3362ba0e4b9f40360
Caitlin Powers <cpowers@6river.com>
2023-09-02 18:02:50 -0400

    madness

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,7 @@ import { ImageGenerator, ImageGeneratorFactory } from "./imageGenerator";
 
 export type Round = {
     prompt: string,
-    image: string | null,
+    image: string,
     player: string
 }
 
@@ -59,7 +59,7 @@ export function addPlayerToGame(game: PendingGame, player: string, prompt: strin
     game.initialPrompts.set(player, prompt);
 }
 
-export function startGame(pending: PendingGame, imageGeneratorFactory: ImageGeneratorFactory) : Game | boolean{
+export function startGame(pending: PendingGame, imageGeneratorFactory: ImageGeneratorFactory) : Game | false {
     const game: Game = {
         status: 'RUNNING',
         players: new Map<string, PlayerUnion>(),
@@ -72,45 +72,59 @@ export function startGame(pending: PendingGame, imageGeneratorFactory: ImageGene
     const playerQueues = new Map<string, PromptStreamQueue>();
     // create positions map
     for (let i=0; i < players.length - 1; i++) {
+        console.log(`Player ${players[i]} passes to ${players[i+1]}`)
         game.nextPlayer.set(players[i], players[i+1]);
-        // all the queues must be created before the image generators
-
     }
-
-    game.nextPlayer.set(players[players.length], players[0]);
+    console.log(`Player ${players[players.length-1]} passes to ${players[0]} ${JSON.stringify(players)}`);
+    game.nextPlayer.set(players[players.length-1], players[0]);
 
     // update queues
-    for (let player in players) {
+    for (let player of players) {
+        console.log(`Creating player queue for ${player}`);
         playerQueues.set(player, []);
     }
 
-    for (let player in players) {
+    for (let player of players) {
         // have to get the player and next player inboxes here for type reasons
         const playerInbox = playerQueues.get(player);
         const nextPlayer = game.nextPlayer.get(player);
 
         if (nextPlayer === undefined ) {
+            console.log(`No next player for player ${player}. Cannot start game.`);
             return false;
         }
 
         const nextPlayerInbox = playerQueues.get(nextPlayer);
 
         if (playerInbox === undefined || nextPlayerInbox === undefined) {
+            console.log(`Either ${player} or ${nextPlayer} is missing inbox. Cannot start game.`);
             return false;
         }
 
         const playerImageGenerator = imageGeneratorFactory(nextPlayerInbox, player);
-        const playerState: PlayerUnion = {
+        // use image generator to initialize inbox for next player
+        const playerInitialPrompt = pending.initialPrompts.get(player);
+        if (playerInitialPrompt === undefined) {
+            console.log(`Player initial prompt for ${player} is undefined, wtf`);
+            return false;
+        }
+        console.log(`Generating initial image for ${player} with prompt ${playerInitialPrompt}`)
+        playerImageGenerator(playerInitialPrompt, [])
+        const playerState: WaitingPlayer = {
             status: 'WAITING',
             inbox: playerInbox,
             playerImageGenerator: playerImageGenerator
         }
-
+        console.log(`Player ${player} state is ${JSON.stringify(playerState)}`);
         game.players.set(player, playerState);
     }
+
+    // now set all players to guessing
+
     console.log(`Done starting game ${JSON.stringify(game)}`);
     return game;
 }
+
 
 export function insertPlayerGuess(player: GuessingPlayer, prompt: string) : WaitingPlayer {
     const playerImageGenerator = player.playerImageGenerator;
--- test/game.spec.ts
+++ test/game.spec.ts
@@ -1,4 +1,4 @@
-import { createNewGame, addPlayerToGame, startGame, insertPlayerGuess, Game } from "../src/game";
+import { createNewGame, addPlayerToGame, startGame, insertPlayerGuess, Game, doInboxCheckForPlayer } from "../src/game";
 import { assert } from 'chai';
 import { mockImageGeneratorFactory } from "./mockImageGenerator";
 
@@ -12,9 +12,10 @@ describe("game", () => {
         const game = createNewGame();
         addPlayerToGame(game, "playerA", "promptA0");
         addPlayerToGame(game, "playerB", "promptB0");
+        addPlayerToGame(game, "playerC", "promptC0");
         const running = startGame(game, mockImageGeneratorFactory);
 
-        assert.isTrue(!!game);
+        assert.isTrue(!!running);
 
         //assert.equal(game.playerPositions.get("playerA"), undefined);
         //assert.equal(game.playerPositions.get("playerB"), "playerA");
@@ -27,7 +28,7 @@ describe("game", () => {
 
         startGame(game);
         assert.equal(game.playerPositions.get("playerA"), "playerB");
-    })
+    })**/
 
     it("should update player position when guess submitted", () => {
         const game = createNewGame();
@@ -36,15 +37,27 @@ describe("game", () => {
         addPlayerToGame(game, "playerC", "promptC0", "imageC0");
         addPlayerToGame(game, "playerD", "promptD0", "imageD0");
 
-        startGame(game);
-        assert.equal(game.playerPositions.get("playerA"), "playerD");
-
+        const running = startGame(game, mockImageGeneratorFactory);
+        
         // player C guesses first
-        insertPlayerGuess(game, "playerC", "guessC1", "imageC1");
+        assert.isTrue(running.status === 'RUNNING', 'Game is not running');
+        // second assert is for type narrowing :woman-shrugging:
+        assert(running.status === 'RUNNING');
+        let playerC = running.players.get("playerC");
+        assert.isTrue(playerC !== undefined, 'Player C is undefined');
+        assert(playerC !== undefined);
+        console.log(`${JSON.stringify(playerC)}`)
+
+        // todo method for going from waiting to guessing
+        assert(playerC.status === 'WAITING')
+        playerC = doInboxCheckForPlayer(playerC);
+        assert.isTrue(playerC.status === 'GUESSING', 'Player C is not guessing');
+        assert(playerC.status === "GUESSING");
+        insertPlayerGuess(playerC, "guessC1");
         
         // player C should have no current items until player D guesses
 
-    })**/
+    })
 
 
 })
