151bef5a181360db93c20e08ff29ede4b720e5bf
Caitlin Powers <cpowers@6river.com>
2023-09-02 18:28:33 -0400

    aboslute insanity

--- test/game.spec.ts
+++ test/game.spec.ts
@@ -1,4 +1,4 @@
-import { createNewGame, addPlayerToGame, startGame, insertPlayerGuess, Game, doInboxCheckForPlayer } from "../src/game";
+import { createNewGame, addPlayerToGame, startGame, insertPlayerGuess, Game, doInboxCheckForPlayer, getImageUrlForGuessingPlayer } from "../src/game";
 import { assert } from 'chai';
 import { mockImageGeneratorFactory } from "./mockImageGenerator";
 
@@ -43,21 +43,23 @@ describe("game", () => {
         assert.isTrue(running.status === 'RUNNING', 'Game is not running');
         // second assert is for type narrowing :woman-shrugging:
         assert(running.status === 'RUNNING');
-        let playerC = running.players.get("playerC");
-        assert.isTrue(playerC !== undefined, 'Player C is undefined');
-        assert(playerC !== undefined);
-        console.log(`${JSON.stringify(playerC)}`)
+        let maybePlayerC = running.players.get("playerC");
+        assert.isTrue(maybePlayerC !== undefined, 'Player C is undefined');
+        assert(maybePlayerC !== undefined);
+        let playerC = maybePlayerC;
+        //console.log(`${JSON.stringify(playerC)}`)
 
         // todo method for going from waiting to guessing
         assert(playerC.status === 'WAITING')
         playerC = doInboxCheckForPlayer(playerC);
         assert.isTrue(playerC.status === 'GUESSING', 'Player C is not guessing');
         assert(playerC.status === "GUESSING");
-        playerC = insertPlayerGuess(playerC, "guessC1");
-        
+        let waitingPlayerC = insertPlayerGuess(playerC, "guessC1");
+        console.log(`Player C after guessing: ${JSON.stringify(playerC)}`)
         // player C should have no current items until player B guesses
-        playerC = doInboxCheckForPlayer(playerC);
-        assert.strictEqual(playerC.status, 'WAITING');
+        let guessingPlayerC = doInboxCheckForPlayer(waitingPlayerC);
+        console.log(`Player C after inbox check: ${JSON.stringify(playerC)}`)
+        assert.strictEqual(guessingPlayerC.status, 'WAITING');
 
         // get player B to guess
         let playerB = running.players.get("playerB");
@@ -69,10 +71,16 @@ describe("game", () => {
         
 
         // check player C inbox again
-        assert(playerC.status === "WAITING");
-        playerC = doInboxCheckForPlayer(playerC);
-
-        assert.equal(playerC.status, "GUESSING");
+        assert(guessingPlayerC.status === "WAITING");
+        let nextPlayerC = doInboxCheckForPlayer(guessingPlayerC);
+        console.log(`Player C after inbox check: ${JSON.stringify(nextPlayerC)}`)
+        assert.equal(nextPlayerC.status, "GUESSING");
+        assert(nextPlayerC.status, "GUESSING")
+        let nextGuessPlayerC = nextPlayerC;
+        const playerCImageUrl = getImageUrlForGuessingPlayer(nextGuessPlayerC);
+        console.log(`${playerCImageUrl}`);
+        
+        
         let playerD = running.players.get("playerD");
         assert(playerD !== undefined && playerD.status === 'WAITING');
         playerD = doInboxCheckForPlayer(playerD);
